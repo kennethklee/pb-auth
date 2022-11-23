3a9916ab8d43789a9be833fd445b0967dd4d7e42
Kenneth Lee <kennethlee@fortinet.com>
2022-11-23 10:39:09 -0500

    test: adds a manual test to make sure 0.8.0 works as intended

---
+++ test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	auth "github.com/kennethklee/pb-auth"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+	var app = pocketbase.New()
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		auth.InstallHeaderAuth(app, e.Router, auth.HeaderAuthConfig{
+			NameHeader:  "X-Forwarded-User",
+			EmailHeader: "X-Forwarded-Email",
+
+			ForceEmail: "test@test.com",
+			ForceName:  "Test Admin",
+		})
+		auth.InstallAPIMeEndpoint(e.Router)
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		panic(err)
+	}
+}
